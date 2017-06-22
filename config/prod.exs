4d6bca7b96dedd95702861fe0dc7d266c385a315
Dominique VASSARD <dominique.vassard@gmail.com>
2017-06-22 22:55:27 +0200

    SSL redirect config

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,6 @@ use Mix.Config
 # which you typically run after static files are built.
 config :artworks, Artworks.Web.Endpoint,
   on_init: {Artworks.Web.Endpoint, :load_from_system_env, []},
-  url: [host: "dominique-vassard-art.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
