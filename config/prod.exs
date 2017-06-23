f61a1cbdd2e91d1a13d2493bb16c94a9d7fc2c9a
Dominique VASSARD <dominique.vassard@gmail.com>
2017-06-23 11:17:50 +0200

    Fix SSL redirect

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :artworks, Artworks.Web.Endpoint,
   on_init: {Artworks.Web.Endpoint, :load_from_system_env, []},
-  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  force_ssl: [host: nil, rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
