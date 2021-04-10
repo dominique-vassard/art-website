cadc27bb00c6373eab7748e76bba1061181c714b
Dominique VASSARD <dominique.vassard@gmail.com>
2021-04-10 18:50:19 +0200

    Force jason usage for plug

--- lib/artworks_web/endpoint.ex
+++ lib/artworks_web/endpoint.ex
@@ -44,7 +44,7 @@ defmodule ArtworksWeb.Endpoint do
   plug Plug.Parsers,
     parsers: [:urlencoded, :multipart, :json],
     pass: ["*/*"],
-    json_decoder: Phoenix.json_library()
+    json_decoder: Jason
 
   plug Plug.MethodOverride
   plug Plug.Head
